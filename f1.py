fd41108fe74deb6519318eced35cac6b7b07d7f7
eena <eenaagrawal08@gmail.com>
2023-06-23 21:08:01 +0530

    chnge in dev

--- f1.py
+++ f1.py
@@ -1,5 +1,10 @@
+l=[]
+l2=[]
 for i in range(1,100):
     if i%2==0:
-        print('even')
+        l.append(i)
     else:
-        print('odd')
+        l2.append(i)
+
+print('even numbers--> ',l)
+print('odd numbers--> ',l2)
